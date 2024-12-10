c88590435d5f7373c8f923306519c4b0e1867746
Wolfgang Rosner <wrosner@tirnet.de>
2024-12-10 22:57:19 +0100

    build index hashes

--- obs_print_vlan_ports.pl
+++ obs_print_vlan_ports.pl
@@ -10,6 +10,8 @@ use Data::Dumper;
 use DBI;
 
 my $debug = 5;
+my $label_device = 21;  # device_ID where the vlan-labels are authoritative
+
 
 our ($hostname, $database, $user, $password);
 require './credentials.in';
@@ -20,6 +22,7 @@ my $dbh = DBI->connect($dsn, $user, $password);
 print "established database connection to $database @ $hostname\n";
 #--------------------------------------------------------------------------
 
+# --- devices ---
 my $dv_sql = <<"EODV";
 SELECT device_id , hostname , sysName , ip 
 FROM devices;
@@ -28,7 +31,10 @@ EODV
 my @devices = retrieve_sql($dv_sql);
 print scalar @devices . " devices: rows found.\n";
 # print '\@devices = ', Dumper(\@devices);
+my %devices_byID = map { ( $_->{'device_id'} , $_  ) } @devices;
+# print '\%devices_byID = ', Dumper(\%devices_byID);
 
+# --- port_vlans ---
 my $pv_sql = <<"EOPV";
 SELECT port_vlan_id, vlan, device_id, port_id 
 FROM ports_vlans 
@@ -39,7 +45,10 @@ EOPV
 my @port_vlans = retrieve_sql($pv_sql);
 print scalar @port_vlans . " port_vlans: rows found.\n";
 # print '\@port_vlans = ', Dumper(\@port_vlans);
+# my %port_vlans_byID = map { ( $_->{'port_vlan_id'} , $_  ) } @port_vlans;
+# print '\%vlans_byID = ', Dumper(\%port_vlans_byID);
 
+# --- ports ---
 my $pt_sql = <<"EOPT";
 SELECT port_id,  port_label , ifIndex, ifType, ifPhysAddress, ifVlan, ifTrunk   
 FROM ports 
@@ -48,18 +57,22 @@ EOPT
 my @ports= retrieve_sql($pt_sql);
 print scalar @ports . " ports: rows found.\n";
 # print '\@ports = ', Dumper(\@ports);
+my %ports_byID = map { ( $_->{'port_id'} , $_  ) } @ports;
+# print '\%ports_byID = ', Dumper(\%ports_byID);
 
-
+# --- vlans ---
 my $vl_sql = <<"EOVL";
-SELECT vlan_ID, vlan_vlan, vlan_name from vlans 
+SELECT vlan_ID, vlan_vlan, vlan_name, device_ID from vlans 
 ORDER by vlan_vlan
 EOVL
 
 my @vlans = retrieve_sql($vl_sql);
 print scalar @vlans . " vlans: rows found.\n";
 # print '\@vlans = ' , Dumper(\@vlans);
+my %vlans_byID = map { ( $_->{'vlan_ID'} , $_  ) } @vlans;
+print '\%vlans_byID = ', Dumper(\%vlans_byID);
 
-
+# my %vlan_names = map { ( $_->{'vlan_ID'} , $_  ) } @vlans;
 
 
 
